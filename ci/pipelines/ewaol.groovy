40cc14641895662621933335ab2448e16e5b005e
Benjamin Mordaunt <89407206+benmordaunt@users.noreply.github.com>
2022-06-17 10:17:00 +0100

    Updates to CI Infrastructure (#9)
    * Restructure the Jenkins CI
    
    Make the CI slightly more modular.
    Also, include builds for virtualization images and the AVA DP.
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I341ed4215511e8cecdc4bfd4f538061743dac638
    
    * Move ewaol function calls into script blocks
    
    Jenkins requires that calls to functions defined in Groovy
    scripts be called in script blocks.
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: Ibc23fc9a5be45b9427b82b5c28d3cd07926582be
    
    * Update ewaol.groovy path.
    
    Only update this for a single machine type to test.
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: Ibc22b33c511eeb09520baad08d0d08c74de288ea
    
    * Allow customization of the kasfile path
    
    For machines whose kas filename does not correspond to the
    machine name as used by kas, allow for customization of this path.
    Also, try an initial port for the avadp-xfce image.
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I6fe1f05adeea6c795246c3fbb05dfeebcb53f3b6
    
    * Remove ".yml" suffix
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I90a55e74ac8ece6b54ad6d815735f199cad87d0b
    
    * Fix typo
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I8aa81b2172dde58a8b20604a6336a5d10b79f94b
    
    * [TEMP] Switch branch for testing
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I31de243bfdee9dae0c72fd3b969b24a98519e47a
    
    * Switch machine from comhpc to ava
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: Ie3e6a31281c4ec4dbfdc6a09fab4de3db6bc5332
    
    * Minor fixups
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I4d73a4e59fa419b542fa85f54324c3316a30480a
    
    * Add ability to specify SDK image type
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: Ibb0d81ac3759a3db325084d60090d011c796bdd6
    
    * Correct variable name
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I0e3224df9edc4cbce735a3c70265a50fe7b27496
    
    * Repair ability to configure an SDK image
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I8c310271838f3cfecf37d313c08173b02f5c6ef1
    
    * Mior fixups
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I8e30cacf80c46659d5774957b93ab6049282315d
    
    * Revise sdk image specification
    
    Because of the way in which the EWAOL distro is structured, it is not possible to use the
    previous approach.
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I0c5d6379891c34aa65cc67d71a18e839ac7cf16e
    
    * Correct search path for wic images
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I983dbdd5351e9b4eb017490694d4ac3c6bc72dd6
    
    * Add constraint for CI that only wic.bz2 images are built
    
    This depends on the underlying BSP layer supporting Kickstart
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: Icd5426fab8282204a480f1c1d496bb9fab25803e
    
    * Add kas header
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I7afa491e08a94bb8335c00e2ecf3fe34bfa94e5c
    
    * Remove old image type setting
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I1b275ba59e23062aef71633bc4d98c3a154891e1
    
    * Mark ava platform as Ok
    
    Signed-off-by: Benjamin Mordaunt <Benjamin.Mordaunt@arm.com>
    Change-Id: I6cd565072ec00ffb256cadc7daed0c63dfdcba83

---
+++ ci/pipelines/ewaol.groovy
@@ -0,0 +1,31 @@
+// Provides the standard structure for a Jenkins EWAOL (declarative) pipeline
+
+def checkoutMachines(Map config) {
+	sh "git clone \"https://github.com/m5p3nc3r/meta-ewaol-machine.git\" --branch ${config.branch}"
+}
+
+def build(Map config) {
+	def kasfileMachineName
+	def sdk = ""
+
+	// If a kas filename is specified, use that. Otherwise, use the machine name.
+	if (config.containsKey("kasfile")) {
+		kasfileMachineName = config.kasfile
+	} else {
+		kasfileMachineName = config.machine
+	}
+	
+	// If sdk image type specified, this affects the yml file composition, but not the TMPDIR
+	if (config.get("sdk", false)) {
+		sdk = "-sdk"
+	}
+
+        dir('meta-ewaol-machine') {
+		sh "kas build kas/ewaol/${config.image}${sdk}.yml:kas/machine/${kasfileMachineName}.yml:kas/ewaol/ci-constraints.yml"
+		if (config.artifactTypes.contains("wic")) {
+                	archiveArtifacts artifacts: "build/tmp_${config.image}/deploy/images/**/*.rootfs.wic.bz2"
+		}
+        }	
+}
+
+return this;
